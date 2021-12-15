9b38a1ed4ca9694d499543fc882968c5496ec0ae
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-12-15 13:43:35 -0700

    commit tree d8a73bce39190536ca0d69200fd0c7e54643ecaf

--- looping.rb
+++ looping.rb
@@ -1,6 +1,28 @@
+require "pry"
+
+# def happy_new_year
+#   10.downto 0 do |num|
+#     puts num
+#     binding.pry
+#   end
+# end
+
+# puts happy_new_year
+
 def happy_new_year
-  # your code here
+  i = 10
+  while i > 0
+    puts i
+    i = i - 1
+    if i == 0 
+      puts "Happy New Year!"
+    end
+  end
 end
+
+puts happy_new_year
+
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
